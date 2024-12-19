7c7cfc16fd0b174d52c88bf3ed2f3bf7f7fe0495
Gaetan Muller <muller.g@odalys-vacances.com>
2024-12-19 23:00:04 +0100

    add vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { inject } from '@vercel/analytics'
 import App from './App.vue'
 import router from './router'
-import './assets/index.css'
+import './assets/main.css'
 
-const pinia = createPinia()
+// Injecte Vercel Analytics
+inject()
+
 const app = createApp(App)
 
-// Configuration de Pinia avant son utilisation
-app.use(pinia)
+app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
