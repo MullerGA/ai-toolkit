12c5d5b32c44504d43b8e7dd1c54fc47c76de593
Gaetan Muller <muller.g@odalys-vacances.com>
2024-12-19 23:01:24 +0100

    fix path asset

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia'
 import { inject } from '@vercel/analytics'
 import App from './App.vue'
 import router from './router'
-import './assets/main.css'
+import './assets/index.css'
 
 // Injecte Vercel Analytics
 inject()
